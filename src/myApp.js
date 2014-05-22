315bc24101976339faa962fb6343d3f5aedb1ab7
Ben Boyle <benjamins.boyle@gmail.com>
2014-05-22 16:18:09 +1000

    watch number limit for changes
    - move controller to new container div
    - add input control for user to set number limit
    - update numbers array when limit changes
    - fix up initialisation of numbers array
    - comments and spacing for clarity in the controller

--- src/myApp.js
+++ src/myApp.js
@@ -1,5 +1,8 @@
 angular.module( 'myApp', [] )
 .controller( 'MultiplicationCtrl', function( $scope, $attrs ) {
+
+
+	// setup array of factors for multiplication
 	function populateNumbers( x ) {
 		var numbers = [];
 		for ( var i = 0; i < x; i++ ) {
@@ -8,9 +11,19 @@ angular.module( 'myApp', [] )
 		return numbers;
 	}
 
-	$scope.numberLimit = $attrs.initialNumberLimit || 10;
-	$scope.numbers = populateNumbers( $scope.numberLimit );
+
+	// compute multiplication (remove logic from view)
 	$scope.compute = function( a, b ) {
 		return a * b;
 	}
+
+
+	// watch number limit for changes and update numbers (and thus table)
+	$scope.$watch( 'numberLimit', function( limit ) {
+		$scope.numbers = populateNumbers( limit );
+	});
+
+
+	// init
+	$scope.numberLimit = $attrs.initialNumberLimit || 10;
 });
