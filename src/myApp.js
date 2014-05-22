43d3b15340257a483a2eacb775aa92380209a56b
Ben Boyle <benjamins.boyle@gmail.com>
2014-05-22 16:35:46 +1000

    hover over row and columns
    - track the active cell (mouseover)
    - set class based on whether cell matches active cell (row or column)
    - orange styling for highlighting

--- src/myApp.js
+++ src/myApp.js
@@ -1,5 +1,13 @@
 angular.module( 'myApp', [] )
 .controller( 'MultiplicationCtrl', function( $scope, $attrs ) {
+	'use strict';
+
+
+	// private
+	var activeFactors = {
+		a: -1,
+		b: -1
+	};
 
 
 	// setup array of factors for multiplication
@@ -15,7 +23,19 @@ angular.module( 'myApp', [] )
 	// compute multiplication (remove logic from view)
 	$scope.compute = function( a, b ) {
 		return a * b;
-	}
+	};
+
+
+	// set factors when mouse moves over table
+	$scope.setActiveFactors = function( a, b ) {
+		activeFactors.a = a;
+		activeFactors.b = b;
+	};
+
+	// compare against active factors
+	$scope.matchesFactor = function( a, b ) {
+		return a === activeFactors.a || b === activeFactors.b;
+	};
 
 
 	// watch number limit for changes and update numbers (and thus table)
